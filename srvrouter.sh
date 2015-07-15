321104fd0d08822f858f62c4da4ead68cf4c2987
Matt Coffin <matt.coffin@flukenetworks.com>
2015-07-15 13:22:53 -0600

    Add config env vars to srvrouter

--- srvrouter.sh
+++ srvrouter.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 while [ true ]; do
 	cp /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.old
-	srvrouter --service nginx --name _webapp._tcp.marathon.mesos -o /etc/haproxy/haproxy.cfg
+	srvrouter --service $SRVROUTER_SERVICE_NAME --name $SRVROUTER_DNS_NAME -o /etc/haproxy/haproxy.cfg
 	changed=$(diff /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.old)
 	if [ ! -z "$changed" ]; then
 		supervisorctl restart haproxy
